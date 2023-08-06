c5a4011d5f9187f545fe840b69b61389dfc632a5
iandre <iandre.vld@gmail.com>
2023-08-06 23:47:34 +0300

    pre-commit

--- src/api/dish/dish_cache.py
+++ src/api/dish/dish_cache.py
@@ -1,11 +1,14 @@
-from fastapi import Depends
-from typing import Annotated, Any
-from pydantic import UUID4
-from aioredis import Redis
-from src.database.base import get_redis_pool
-from .repositories import Dish
-from src.config import EXPIRATION, Keys
 import pickle
+from typing import Annotated
+
+from aioredis import Redis
+from fastapi import Depends
+from pydantic import UUID4
+
+from src.config import EXPIRATION, Keys
+from src.database.base import get_redis_pool
+
+from .repositories import Dish
 
 
 class DishCacheRepository:
@@ -18,41 +21,40 @@ class DishCacheRepository:
         self.key_menu_list = Keys.key_menu_list.value
         self.key_dish_list_prefix = Keys.key_dish_list_prefix.value
         self.key_dish_prefix = Keys.key_dish_prefix.value
-    
-    async def get_dish_list_cache(self, submenu_id: UUID4)-> list[Dish | None] | None:
-        if cache:= await self.redis.get(f'{self.key_dish_list_prefix}{str(submenu_id)}'):
+
+    async def get_dish_list_cache(self, submenu_id: UUID4) -> list[Dish | None] | None:
+        if cache := await self.redis.get(f'{self.key_dish_list_prefix}{str(submenu_id)}'):
             return pickle.loads(cache)
         return None
-    
+
     async def set_dish_list_cache(self, submenu_id: UUID4, data: list[Dish]) -> None:
         await self.redis.set(f'{self.key_dish_list_prefix}{str(submenu_id)}',
                              pickle.dumps(data),
                              ex=self.ex)
         return None
-    
+
     async def get_dish_cache(self, dish_id: UUID4) -> Dish | None:
-        if cache:= await self.redis.get(f'{self.key_dish_prefix}{str(dish_id)}'):
+        if cache := await self.redis.get(f'{self.key_dish_prefix}{str(dish_id)}'):
             return pickle.loads(cache)
         return None
-    
+
     async def set_dish_cache(self, dish_id: UUID4, data: Dish) -> None:
         await self.redis.set(f'{self.key_dish_prefix}{str(dish_id)}',
-                                pickle.dumps(data),
-                                ex=self.ex)
+                             pickle.dumps(data),
+                             ex=self.ex)
         return None
-    
+
     async def delete_dish_cache(self, dish_id: UUID4) -> None:
         await self.redis.delete(f'{self.key_dish_prefix}{str(dish_id)}')
         await self.redis.delete(f'{self.key_menu_list}')
         keys_submenu: list = await self.redis.keys(f'{self.key_prefix_submenu}*')
         keys_menu: list = await self.redis.keys(f'{self.key_prefix_menu}*')
         keys_menu.extend(keys_submenu)
-        if keys_menu :
+        if keys_menu:
             await self.redis.delete(*keys_menu)
 
-    
     async def delete_dish_list_cache(self) -> None:
-        if keys:= await self.redis.keys(f'{self.key_dish_list_prefix}*'):
+        if keys := await self.redis.keys(f'{self.key_dish_list_prefix}*'):
             await self.redis.delete(*keys)
 
     async def delete_cache(self, submenu_id) -> None:
